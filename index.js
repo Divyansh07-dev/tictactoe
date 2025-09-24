bc9ea97f84587c8fb07486df7f6d13debf8eb8ce
Divyansh07-dev <divyanshsharma1972005@gmail.com>
2025-09-24 19:03:32 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,38 +1,21 @@
 let turn = 'O';
 let total_turn = 0;
-
-
 let winner = [
     [0,1,2],[3,4,5],[6,7,8],
     [0,3,6],[1,4,7],[2,5,8],
     [0,4,8],[2,4,6]
 ];
-
 let board_array = new Array(9).fill("E");
-//   0.  1.  2.  3.  4.  5.  6.  7.  8
-// ["E","E","E","E","E","E","E","E","E"]
-
-
 function checkWinner(){
-   
-    for(let [index0,index1,index2] of winner)
+       for(let [index0,index1,index2] of winner)
     {
         if(board_array[index0]!="E"&&board_array[index0]===board_array[index1]&&board_array[index1]===board_array[index2])
             return 1;
     }
-
-
-    return 0;
-
+   return 0;
 }
-
-
-
-// Print sahi se ho rha hai....
-
 const printer = (event)=>{
-   
-    const element = event.target;
+   const element = event.target;
     // if board is empty
     if(board_array[element.id]==="E") 
    {
@@ -45,7 +28,7 @@ const printer = (event)=>{
         {
             document.getElementById('winningMessage').innerHTML = "Winner is O";
             board.removeEventListener('click',printer);
-            return;
+            return; // logic 
         }
         turn = "X";
     }
@@ -60,39 +43,25 @@ const printer = (event)=>{
         }
         turn = "O";
     }
-
     if(total_turn==9)
     {
         document.getElementById('winningMessage').innerHTML = "Match is Draw";
         board.removeEventListener('click',printer);
     }
-
-  }   
+ }   
 }
-
-
-
-
 const board = document.querySelector('.board');
 board.addEventListener('click',printer);
-
-
 const Restart = document.getElementById("restartButton");
 Restart.addEventListener('click',()=>{
    const cell = document.getElementsByClassName('cell');
-
    Array.from(cell).forEach((value)=>{
      value.innerHTML = "";
    })
-
-
-   turn = "O";
+ turn = "O";
    total_turn = 0;
    board_array = new Array(9).fill("E");
    document.getElementById('winningMessage').innerHTML = "";
    board.addEventListener('click',printer);
 
 })
-
-
-// Homework Project: Rock paper scissor
